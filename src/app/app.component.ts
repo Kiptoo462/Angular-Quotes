d45af203a4e127ab89fb21e144db47b6da27b9df
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-07-20 15:08:58 +0300

    Added app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  title = 'Famous Quotes';
+
+  quote = [
+    new Quotes(1, '`Place of destiny` is our current book. Please vote for us, your vote and comments will helps us improve our standards.', 'Marjorie Oludhe Macgoye', 0,0, 'Paulines Publications Africa', new Date(2021, 7, 18) ),
+  ]
 }
