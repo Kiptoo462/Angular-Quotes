11e7a99aa2c44e017e2f40c4e6c927d24c5f602d
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-07-20 15:15:05 +0300

    Added  forms to quote- component

---
+++ src/app/quoteform/quoteform.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quotes } from '../quotes';
+
+@Component({
+  selector: 'app-quoteform',
+  templateUrl: './quoteform.component.html',
+  styleUrls: ['./quoteform.component.css']
+})
+export class QuoteformComponent implements OnInit {
+
+  newQuotes = new Quotes(0, '', '', 0, 0,``, new Date());
+  @Output() addQuote = new EventEmitter<Quotes>();
+
+    submitQuote() {
+    this.addQuote.emit(this.newQuotes);
+    this.newQuotes = new Quotes(0, '', '', 0,0, ``, new Date());
+
+    }
+    
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
